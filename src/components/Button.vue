41817d4e711b6f9861fffc865081ea0294a48d02
unknown <A0317@racing.com.tw>
2023-07-06 13:24:04 +0800

    second commit

---
+++ src/components/Button.vue
@@ -0,0 +1,19 @@
+<template>
+    <!-- @click is sh of v-on:click -->
+    <button  @click="onClick()" :style="{background:color}" class="btn">{{text}}</button>
+</template>
+
+<script>
+    export default {
+        name: 'Button',
+        props: {
+            text: String,
+            color: String
+        },
+        methods: {
+            onClick() {
+                this.$emit('btn-click')
+            }
+        }
+    }
+</script>
